580e13f97b5b0d97fe85916c397a30e951535682
Martin Scheidt <m.scheidt@tu-bs.de>
2022-04-28 17:23:23 +0200

    added docs and GitHub actions from Julia template

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using TrainRun
+using Documenter
+
+DocMeta.setdocmeta!(TrainRun, :DocTestSetup, :(using TrainRun); recursive=true)
+
+makedocs(;
+    modules=[TrainRun],
+    authors="Max Kannenberg, Martin Scheidt, and contributors",
+    repo="https://github.com/railtoolkit/TrainRun.jl/blob/{commit}{path}#{line}",
+    sitename="TrainRun.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://railtoolkit.github.io/TrainRun.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/railtoolkit/TrainRun.jl",
+    devbranch="master",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,14 @@
+```@meta
+CurrentModule = TrainRun
+```
+
+# TrainRun
+
+Documentation for [TrainRun](https://github.com/railtoolkit/TrainRun.jl).
+
+```@index
+```
+
+```@autodocs
+Modules = [TrainRun]
+```

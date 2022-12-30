ef88e7b56f8ddb738de2914c60a4e85f3e0afe2a
Martin Scheidt <m.scheidt@tu-bs.de>
2022-12-30 18:22:44 +0100

    improved automated documentation generation

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(TrainRuns, :DocTestSetup, :(using TrainRuns); recursive=true
 makedocs(;
     modules=[TrainRuns],
     authors="Max Kannenberg, Martin Scheidt, and contributors",
-    repo="https://github.com/railtoolkit/TrainRuns.jl/blob/{commit}{path}#{line}",
+    repo="https://www.railtoolkit.org/TrainRuns.jl//blob/{commit}{path}#{line}",
     sitename="TrainRuns.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://railtoolkit.github.io/TrainRuns.jl",
+        canonical="https://www.railtoolkit.org/TrainRuns.jl",
         assets=String[],
     ),
     pages=[

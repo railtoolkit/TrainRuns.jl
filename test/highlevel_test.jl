02ea6374bddf2295337af224aba4e2d08968bfbb
Martin Scheidt <142348+kaat0@users.noreply.github.com>
2024-07-01 21:58:12 +0200

    switched default trainrun() test to df_snapshot_test macro

--- test/highlevel_test.jl
+++ test/highlevel_test.jl
@@ -7,33 +7,6 @@
 
 using CSV, DataFrames
 
-"""
-    @df_snapshot_test(name, expr::Expr)
-
-Saves DataFrames to CSV and compares them.
-To renew snapshot delete the file named like the test.
-"""
-macro df_snapshot_test(dir, name, expr::Expr)
-    return quote
-        filename = $(esc(name)) * ".csv"
-        filepath = joinpath(Base.source_dir(), "snapshots", $(dir), filename)
-        df = $(esc(expr))
-
-        if !isfile(filepath)
-            if !isdir(dirname(filepath))
-                mkdir(dirname(filepath))
-            end
-
-            CSV.write(filepath, df, missingstring = "!missing!")
-            @warn "Snapshot file $filename created"
-        end
-
-        csv_df = DataFrame(CSV.File(filepath; missingstring = "!missing!"))
-
-        @test df == csv_df
-    end
-end
-
 trains = Dict()
 paths = Dict()
 settings = Dict()
@@ -68,31 +41,6 @@ end
 
 tests = Base.Iterators.product(trains, paths)
 
-## routine to generate the anticipated Dict()
-# anticipated = Dict()
-# for test in tests
-#   @info test[1][1],"-",test[2][1]
-#   result = @time trainrun(test[1][2],test[2][2])
-#   push!(anticipated, Symbol(String(test[1][1]) * "_" * String(test[2][1])) => result )
-# end
-
-anticipated = Dict(
-    :default => Dict(
-    :freight_const => 746.6594691660882,
-    :freight_slope => 842.3797947097586,
-    :freight_speed => 751.9727357301351,
-    :freight_realworld => 8789.299477891092,
-    :local_const => 392.6801497584646,
-    :local_slope => 396.59291844946534,
-    :local_speed => 524.3734475900396,
-    :local_realworld => 3438.3543735577446,
-    :longdistance_const => 330.8511578156266,
-    :longdistance_slope => 331.7163794230447,
-    :longdistance_speed => 501.13448446081713,
-    :longdistance_realworld => 2913.3759609192407
-),
-)
-
 @testset "trainrun()" begin
     @testset "Default settings" begin
         for test in tests
@@ -101,22 +49,20 @@ anticipated = Dict(
             test_name::String = String(train[1]) * "_" * String(path[1])
 
             @testset "$test_name" begin
-                result = trainrun(test[1][2], test[2][2])[end, :t]
-                expected = anticipated[:default][Symbol(test_name)]
-                # compare result to test data set
-                @test isapprox(result, expected, rtol = 0.01)
+                @df_snapshot_test "default/$test_name" trainrun(
+                    train[2], path[2])
             end
         end
     end
 
-    @testset "POIs" begin
+    @testset "POIs settings" begin
         for test in tests
             train = test[1]
             path = test[2]
             test_name::String = String(train[1]) * "_" * String(path[1])
 
             @testset "$test_name" begin
-                @df_snapshot_test "poi" "$test_name" trainrun(
+                @df_snapshot_test "poi/$test_name" trainrun(
                     train[2], path[2], settings["poi"])
             end
         end
